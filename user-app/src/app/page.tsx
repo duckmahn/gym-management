107f1eb4c5de8bb00706c46e687238d68d9c2a28
Manh <130069804+duckmahn@users.noreply.github.com>
2024-10-05 10:53:50 +0700

    config project

--- user-app/src/app/page.tsx
+++ user-app/src/app/page.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import Hom1 from '@/app/userLogin';
-import SignUp from '@/app/signUp';
-import Dashboard from '@/dashboard';
+import Image from "next/image"
+import Dashboard from "@/dashboard"
 
 export default function Home() {
-  return (
-    <Dashboard/>
-  );
+  return <Dashboard />
 }
