dd2642b7d518ce61d4adad395f365cab1fec1a29
Manh <130069804+duckmahn@users.noreply.github.com>
2024-10-09 16:58:29 +0700

    .

--- user-app/src/app/Dashboard/page.tsx
+++ user-app/src/app/Dashboard/page.tsx
@@ -5,33 +5,33 @@ export default function Dashboard() {
         <h1 className="text-center my-2.5 text-white">GYMHUFLIT</h1>
         <hr />
         <ul className="list-none">
-          <li className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
+          <button className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
             Management
-          </li>
-          <li className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
+          </button>
+          <button className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
             Calendar
-          </li>
-          <li className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
+          </button>
+          <button className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
             Coach
-          </li>
-          <li className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
+          </button>
+          <button className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
             Finance
-          </li>
-          <li className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
+          </button>
+          <button className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
             Factility
-          </li>
-          <li className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
+          </button>
+          <button className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
             Customer
-          </li>
-          <li className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
+          </button>
+          <button className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
             Settings
-          </li>
-          <li className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
+          </button>
+          <button className="inline-block w-full h-full p-2 border-none rounded-md text-white hover:bg-brown-500">
             Sign Out
-          </li>
+          </button>
         </ul>
       </div>
       <div className="inline-block w-4/5 h-full bg-white p-2.5"></div>
     </div>
-  );
+  )
 }
