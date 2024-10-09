39d26481203a0ab1713a16c1c41d4bd4fd7f4c6b
GS-755 <phananhtien2709@gmail.com>
2024-10-09 16:31:54 +0700

    change syntax

--- api/gym-management-api/GymManagement_API/obj/Debug/net8.0/GymManagement_API.AssemblyInfo.cs
+++ api/gym-management-api/GymManagement_API/obj/Debug/net8.0/GymManagement_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymManagement_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+395fb6103cb9303a01617a34eefbe2138e373a33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a704e28e845fb14013642256b62145de48a64bd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymManagement_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymManagement_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
