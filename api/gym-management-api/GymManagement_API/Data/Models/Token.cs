35bef17d747cbfb42ecdbde2158021c53aafa6d1
Manh <130069804+duckmahn@users.noreply.github.com>
2024-10-19 17:48:59 +0700

    directory nhu lon

--- api/gym-management-api/GymManagement_API/Data/Models/Token.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GymManagement_API.Data.Models
-{
-    public class Token
-    {
-        public Guid Id { get; set; }
-        public string Email { get; set; }
-        public string Username { get; set; }
-    }
-}
