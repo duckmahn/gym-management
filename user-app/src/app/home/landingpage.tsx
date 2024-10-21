cff07ee9f19964c6968569bf9b90528303a68e16
LeQuocDam <150604524+LeQuocDam@users.noreply.github.com>
2024-10-21 17:04:30 +0700

    update landingpage

--- user-app/src/app/home/landingpage.tsx
+++ user-app/src/app/home/landingpage.tsx
@@ -4,13 +4,13 @@ import "./landingpage.css"
 export default function LandingPage() {
   return (
     <div className="landingpage">
-      <div>
-        <div className="tiltle">
+      <div className="first">
+        <div className="itemf1">
           <h2>HUFLITGYM</h2>
           <p>Transform Your Body</p>
         </div>
         <button className="search">Search</button>
-        <ul>
+        <ul className="itemf2">
           <li>Home</li>
           <li>Program</li>
           <li>Coaching</li>
@@ -22,10 +22,10 @@ export default function LandingPage() {
       </div>
       <div className="second">
         <div className="left">
-          <p>Achive Your</p>
-          <p className="outstanding">FITNESS GOALS</p>
-          <p>With FitMaker</p>
-          <div className="describe">
+          <p className="itemsl1">Achive Your</p>
+          <p className="itemsl2">FITNESS GOALS</p>
+          <p className="itemsl1">With FitMaker</p>
+          <div className="itemsl3">
             "Join The Fitmaker Community And Transform Your Fitness Journey. Our
             Expert Coaches And <br />
             Personalized Programs Are Designed To Help You Achive Your Goals And
@@ -37,10 +37,12 @@ export default function LandingPage() {
         </div>
       </div>
       <div className="third">
-        <div className="item">
-          <span>96%</span>
-          <div className="tiltle">Client Satisfaction</div>
-          <div className="summary">
+        <div className="itemt1">
+          <div className="itemt11">
+            <div>96%</div>
+            <div className="tiltle">Client Satisfaction</div>
+          </div>
+          <div className="itemt12">
             Our Members Love Their Results And Experience
           </div>
         </div>
@@ -152,6 +154,7 @@ export default function LandingPage() {
           <a href="#">Learn More</a>
         </div>
       </div>
+
       <div className="five">
         <div className="first">
           <div className="first1">Our Plans</div>
@@ -166,6 +169,29 @@ export default function LandingPage() {
           </div>
         </div>
         <div className="second">
+          <div className="item">
+            <div className="item1">Package</div>
+            <div className="item2">PRO PLAN</div>
+            <div className="item3">Description</div>
+            <div className="item6">
+              Our Pro Plan Offers Advanced Workouts And Personalized Nutrition
+              Coaching To Help You Reach Your Goals Faster. Sign Up Right Now
+            </div>
+            <div className="item3">Features</div>
+            <div className="item4">
+              <ul>
+                <li>Access To All Of Our Exercise Videos</li>
+                <li>Progress Tracking</li>
+                <li>Supportive Online Community</li>
+                <li>Advanced, Personalized Workout Plans</li>
+                <li>Comprehensive Nutrition Coaching</li>
+                <li>Access To Advanced Workout Programs</li>
+                <li>Body Composition Analysis</li>
+              </ul>
+            </div>
+            <div className="item5">99$/USTD</div>
+            <button>Choose This Plan</button>
+          </div>
           <div className="item">
             <div className="item1">Package</div>
             <div className="item2">PRO PLAN</div>
@@ -186,19 +212,19 @@ export default function LandingPage() {
                 <li>Body Composition Analysis</li>
               </ul>
             </div>
-            <button>Choose This Plan</button>
             <div className="item5">99$/USTD</div>
+            <button>Choose This Plan</button>
           </div>
           <div className="item">
-            <div>Package</div>
-            <div>PRO PLAN</div>
-            <div>Description</div>
+            <div className="item1">Package</div>
+            <div className="item2">BEGINNER PLAN</div>
+            <div className="item3">Description</div>
             <div>
-              Our Pro Plan Offers Advanced Workouts And Personalized Nutrition
-              Coaching To Help You Reach Your Goals Faster. Sign Up Right Now
+              Start Your Fitness Journey With Our Beginner Plan. Built A Strong
+              Foundation With Basic Workouts And Essential
             </div>
-            <div>Features</div>
-            <div>
+            <div className="item3">Features</div>
+            <div className="item4">
               <ul>
                 <li>Access To All Of Our Exercise Videos</li>
                 <li>Progress Tracking</li>
@@ -209,29 +235,47 @@ export default function LandingPage() {
                 <li>Body Composition Analysis</li>
               </ul>
             </div>
-            <div>99$/USTD</div>
+            <div className="item5">99$/USTD</div>
+            <button>Choose This Plan</button>
           </div>
-          <div className="item">
-            <div>Package</div>
-            <div>PRO PLAN</div>
-            <div>Description</div>
-            <div>
-              Our Pro Plan Offers Advanced Workouts And Personalized Nutrition
-              Coaching To Help You Reach Your Goals Faster. Sign Up Right Now
-            </div>
-            <div>Features</div>
-            <div>
-              <ul>
-                <li>Access To All Of Our Exercise Videos</li>
-                <li>Progress Tracking</li>
-                <li>Supportive Online Community</li>
-                <li>Advanced, Personalized Workout Plans</li>
-                <li>Comprehensive Nutrition Coaching</li>
-                <li>Access To Advanced Workout Programs</li>
-                <li>Body Composition Analysis</li>
-              </ul>
-            </div>
-            <div>99$/USTD</div>
+        </div>
+      </div>
+
+      <div className="six">
+        <div className="items1">
+          <div className="items11">
+            <div className="items111">Our Fitness </div>
+            <div className="items112">Tools</div>
+          </div>
+          <div className="items12">
+            <button>Previous</button>
+            <button>Next</button>
+          </div>
+        </div>
+        <div className="items2">
+          Access A Variety Of Tools To Help You Reach Your Fitness Goals More
+          Effectively
+        </div>
+        <div className="items3">
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
+          </div>
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
+          </div>
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
+          </div>
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
+          </div>
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
           </div>
         </div>
       </div>
