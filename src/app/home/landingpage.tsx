cd162fc43b3bf8492a1c1370656ec619468b0c51
LeQuocDam <150604524+LeQuocDam@users.noreply.github.com>
2024-10-23 17:45:12 +0700

    update landingpage

--- src/app/home/landingpage.tsx
+++ src/app/home/landingpage.tsx
@@ -4,36 +4,327 @@ import "./landingpage.css"
 export default function LandingPage() {
   return (
     <div className="landingpage">
-      <div>
-        <div className="tiltle">
-          <h2>HUFLITGYM</h2>
-          <p>Transform Your Body</p>
+      <div className="first">
+        <div className="once">
+          <div className="top">HUFLITGYM</div>
+          <div className="bot">Transform Your Body</div>
         </div>
-        <button className="search">Search</button>
-        <ul>
-          <li>Home</li>
-          <li>Program</li>
-          <li>Coaching</li>
-          <li>Membership</li>
-          <li>About us</li>
-        </ul>
-        <button className="authencation">Login</button>
-        <button className="authencation">Sign Up</button>
+        <div className="two">
+          <button>Search</button>
+        </div>
+        <div className="three">
+          <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>Coaching</li>
+            <li>Membership</li>
+            <li>About us</li>
+          </ul>
+        </div>
+        <div className="four">
+          <button>Log In</button>
+          <button>Sign Up</button>
+        </div>
       </div>
+
       <div className="second">
         <div className="left">
-          <p>Achive Your</p>
-          <p className="outstanding">FITNESS GOALS</p>
-          <p>With FitMaker</p>
-          <div className="describe">
-            "Join The Fitmaker Community And Transform Your Fitness Journey. Our
-            Expert Coaches And <br />
-            Personalized Programs Are Designed To Help You Achive Your Goals And
-            Exceed Your <br />
-            Expectations. Ready To Make A Change?"
+          <div className="top">
+            <div className="false">Achive Your</div>
+            <div className="true">FITNESS GOALS</div>
+            <div className="false">With FitMaker</div>
           </div>
-          <button>Start Your Journey</button>
-          <button>Explore Programs</button>
+          <div className="mid">
+            "Join The Fitmaker Community And Transform Your Fitness Journey. Our
+            Expert Coaches And Personalized Programs Are Designed To Help You
+            Achive Your Goals And Exceed Your Expectations. Ready To Make A
+            Change?"
+          </div>
+          <div className="bot">
+            <button>Start Your Journey</button>
+            <button>Explore Programs</button>
+          </div>
+        </div>
+        <div className="right">
+          <img src="/images/img1.png" />
+        </div>
+      </div>
+
+      <div className="third">
+        <div className="zero">
+          <div className="top">
+            <div className="left">96%</div>
+            <div className="right">Client Satisfaction</div>
+          </div>
+          <div className="bot">
+            <p>Our Members Love Their Results And Experience</p>
+          </div>
+        </div>
+        <div className="zero">
+          <div className="top">
+            <div className="left">+5</div>
+            <div className="right">Years Of Experience</div>
+          </div>
+          <div className="bot">
+            <p>Trust In Our Proven Track Record Of Transforming</p>
+          </div>
+        </div>
+        <div className="zero">
+          <div className="top">
+            <div className="left">+800</div>
+            <div className="right">Active Members</div>
+          </div>
+          <div className="bot">
+            <p>Join Our Thriving Fitness Community</p>
+          </div>
+        </div>
+        <div className="zero">
+          <div className="top">
+            <div className="left">24/7</div>
+            <div className="right">Support Available</div>
+          </div>
+          <div className="bot">
+            <p>Expert Assistance Whenever You Need It</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="four">
+        <div className="top">
+          <div className="left">Our </div>
+          <div className="right">Services</div>
+        </div>
+        <div className="mid">
+          At This Part You Can Easily Access All Of Our Services. Take A Look At
+          Them And Choose Ever You Want.
+        </div>
+        <div className="bot">
+          <div className="zero">
+            <div className="top">
+              <img src="/images/four/four1.png" />
+            </div>
+            <div className="bot">
+              <a href="#">Learn More</a>
+            </div>
+          </div>
+          <div className="zero">
+            <div className="top">
+              <img src="/images/four/four2.png" />
+            </div>
+            <div className="bot">
+              <a href="#">Learn More</a>
+            </div>
+          </div>
+          <div className="zero">
+            <div className="top">
+              <img src="/images/four/four3.png" />
+            </div>
+            <div className="bot">
+              <a href="#">Learn More</a>
+            </div>
+          </div>
+          <div className="zero">
+            <div className="top">
+              <img src="/images/four/four4.png" />
+            </div>
+            <div className="bot">
+              <a href="#">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="five">
+        <div className="first">
+          <div className="first1">Our Plans</div>
+          <div className="first2">
+            Select The Plan That Suits Your Fitness Goals And Let Our Expert
+            Coaches Guide You Every Step Of The Way
+          </div>
+          <div className="first3">
+            <label>
+              <input type="checkbox" />
+            </label>
+          </div>
+        </div>
+        <div className="second">
+          <div className="item">
+            <div className="item1">Package</div>
+            <div className="item2">PRO PLAN</div>
+            <div className="item3">Description</div>
+            <div className="item6">
+              Our Pro Plan Offers Advanced Workouts And Personalized Nutrition
+              Coaching To Help You Reach Your Goals Faster. Sign Up Right Now
+            </div>
+            <div className="item3">Features</div>
+            <div className="item4">
+              <ul>
+                <li>Access To All Of Our Exercise Videos</li>
+                <li>Progress Tracking</li>
+                <li>Supportive Online Community</li>
+                <li>Advanced, Personalized Workout Plans</li>
+                <li>Comprehensive Nutrition Coaching</li>
+                <li>Access To Advanced Workout Programs</li>
+                <li>Body Composition Analysis</li>
+              </ul>
+            </div>
+            <div className="item5">99$/USTD</div>
+            <button>Choose This Plan</button>
+          </div>
+          <div className="item">
+            <div className="item1">Package</div>
+            <div className="item2">PRO PLAN</div>
+            <div className="item3">Description</div>
+            <div>
+              Our Pro Plan Offers Advanced Workouts And Personalized Nutrition
+              Coaching To Help You Reach Your Goals Faster. Sign Up Right Now
+            </div>
+            <div className="item3">Features</div>
+            <div className="item4">
+              <ul>
+                <li>Access To All Of Our Exercise Videos</li>
+                <li>Progress Tracking</li>
+                <li>Supportive Online Community</li>
+                <li>Advanced, Personalized Workout Plans</li>
+                <li>Comprehensive Nutrition Coaching</li>
+                <li>Access To Advanced Workout Programs</li>
+                <li>Body Composition Analysis</li>
+              </ul>
+            </div>
+            <div className="item5">99$/USTD</div>
+            <button>Choose This Plan</button>
+          </div>
+          <div className="item">
+            <div className="item1">Package</div>
+            <div className="item2">BEGINNER PLAN</div>
+            <div className="item3">Description</div>
+            <div>
+              Start Your Fitness Journey With Our Beginner Plan. Built A Strong
+              Foundation With Basic Workouts And Essential
+            </div>
+            <div className="item3">Features</div>
+            <div className="item4">
+              <ul>
+                <li>Access To All Of Our Exercise Videos</li>
+                <li>Progress Tracking</li>
+                <li>Supportive Online Community</li>
+                <li>Advanced, Personalized Workout Plans</li>
+                <li>Comprehensive Nutrition Coaching</li>
+                <li>Access To Advanced Workout Programs</li>
+                <li>Body Composition Analysis</li>
+              </ul>
+            </div>
+            <div className="item5">99$/USTD</div>
+            <button>Choose This Plan</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="six">
+        <div className="items1">
+          <div className="items11">
+            <div className="items111">Our Fitness </div>
+            <div className="items112">Tools</div>
+          </div>
+          <div className="items12">
+            <button>Previous</button>
+            <button>Next</button>
+          </div>
+        </div>
+        <div className="items2">
+          Access A Variety Of Tools To Help You Reach Your Fitness Goals More
+          Effectively
+        </div>
+        <div className="items3">
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
+          </div>
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
+          </div>
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
+          </div>
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
+          </div>
+          <div className="items31">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtnvTY7SelArPsbFqugw_bD9jFpmnJ0mtSA&s" />
+            <a href="#">Learn More</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="seven">
+        <div className="se1">
+          <div className="se11">
+            <div className="se111">What Our </div>
+            <div className="se112">Customers Say</div>
+          </div>
+          <div className="se12">
+            At This Part You Can See Few Of The Many Positive Reviews Of Our
+            Customers
+          </div>
+        </div>
+      </div>
+
+      <div className="ten">
+        <div className="ten-left">
+          <div className="left-top">
+            <div className="top-1">Join Our </div>
+            <div className="top-2">Fitness Community</div>
+          </div>
+          <div className="left-mid">
+            Sign Up Now To Unlock Exlusive Access To Personalized Workout Plans,
+            Expert Coaching, And A Supportive Community That Will Help You
+            Achieve Your Fitness Goals
+          </div>
+          <div className="left-bot">
+            <div className="bot-item">
+              <div className="item-top">
+                <div className="top-left">Join Our </div>
+                <div className="top-right">Fitness Community</div>
+              </div>
+              <div className="item-bot">
+                Customized Routines That Match Your Fitness Level And Goals,
+                Ensuring You Achieve The Best Results In The Most Efficient Way.
+              </div>
+            </div>
+            <div className="bot-item">
+              <div className="item-top">
+                <div className="top-left">Join Our </div>
+                <div className="top-right">Fitness Community</div>
+              </div>
+              <div className="item-bot">
+                Customized Routines That Match Your Fitness Level And Goals,
+                Ensuring You Achieve The Best Results In The Most Efficient Way.
+              </div>
+            </div>
+            <div className="bot-item">
+              <div className="item-top">
+                <div className="top-left">Join Our </div>
+                <div className="top-right">Fitness Community</div>
+              </div>
+              <div className="item-bot">
+                Customized Routines That Match Your Fitness Level And Goals,
+                Ensuring You Achieve The Best Results In The Most Efficient Way.
+              </div>
+            </div>
+            <div className="bot-item">
+              <div className="item-top">
+                <div className="top-left">Join Our </div>
+                <div className="top-right">Fitness Community</div>
+              </div>
+              <div className="item-bot">
+                Customized Routines That Match Your Fitness Level And Goals,
+                Ensuring You Achieve The Best Results In The Most Efficient Way.
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
