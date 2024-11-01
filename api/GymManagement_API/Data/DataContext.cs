bc6ca16c388a19d05effde581abbb9311e001ecb
duckmahn <21dh113296@st.huflit.edu.vn>
2024-11-01 22:36:52 +0700

    fix conflict

--- api/GymManagement_API/Data/DataContext.cs
+++ api/GymManagement_API/Data/DataContext.cs
@@ -19,6 +19,7 @@ namespace GymManagement_API.Data
         public DbSet<Schedule> Schedules { get; set; }
         public DbSet<Facilities> Facilities { get; set; }
         public DbSet<BookingTrainer> BookingTrainers { get; set; }
+        public DbSet<Courses> Courses { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
