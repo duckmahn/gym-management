ce9791ba0db9a04e7f74ae5ea77efd3d261ae76a
duckmahn <21dh113296@st.huflit.edu.vn>
2024-11-14 23:48:18 +0700

    fix noti

--- admin-app/src/components/header.tsx
+++ admin-app/src/components/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Dialog, DialogContent, DialogFooter, DialogTitle } from "./ui/dialog";
+import { Dialog, DialogContent, DialogFooter, DialogHeader } from "./ui/dialog";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
@@ -10,45 +10,46 @@ import { NEXT_PUBLIC_API_URL } from "../../apiconfig";
 import { Textarea } from "./ui/textarea";
 
 interface FormData {
-  title: string | null;
-  content: string | null;
+  title: string;
+  content: string;
 }
 
 export default function Header(): JSX.Element {
   const [isOpen, setIsOpen] = useState(false);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const [formData, setFormData] = useState<FormData>({
+    title: "",
+    content: "",
+  });
 
-  const sendNotification = () => {
-    if (!!content && !!title) {
-      sendNotiofication({ title, content });
-      setTitle("");
-      setContent("");
+  const sendNotification = async (e) => {
+    e.preventDefault();
+
+    if (formData.title && formData.content) {
+      await sendNotificationRequest(formData);
+      setIsOpen(false);
     }
-    setIsOpen(false);
   };
 
-  const sendNotiofication = (request: FormData) => {
-    const res = axios.post(
+  const sendNotificationRequest = async (request: FormData) => {
+    const res = await axios.post(
       `${NEXT_PUBLIC_API_URL}/api/Notification/notification`,
       request
     );
-    console.log("🚀 ~ sendNotiofication ~ res:", res);
-    return res;
+    return res.statusText;
   };
 
   return (
     <header className="flex justify-between items-center mb-5 relative">
       <div className="flex items-center justify-center w-full relative">
         <div
-          className="text-2xl mr-2.5 cursor-pointer hover:scale-125 "
+          className="text-2xl mr-2.5 cursor-pointer hover:scale-125"
           onClick={() => setIsOpen(true)}
         >
           <i className="fas fa-bell text-[#c21f37]" />
         </div>
         <div className="flex">
           <input
-            className="w-[400px] p-2.5 rounded-full border border-gray-300 pl-10 bg-no-repeat bg-center "
+            className="w-[400px] p-2.5 rounded-full border border-gray-300 pl-10 bg-no-repeat bg-center"
             type="text"
             placeholder="Search here"
           />
@@ -56,42 +57,44 @@ export default function Header(): JSX.Element {
 
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
           <DialogContent>
-            <DialogTitle>Send Notification</DialogTitle>
-
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right font-bold">
-                  Title
-                </Label>
-                <Input
-                  id="title"
-                  placeholder="Title"
-                  className="col-span-3"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
-                />
+            <DialogHeader className="font-bold">Send Notification</DialogHeader>
+            <form onSubmit={sendNotification}>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="title" className="text-right font-bold">
+                    Title
+                  </Label>
+                  <Input
+                    id="title"
+                    placeholder="Title"
+                    className="col-span-3"
+                    value={formData.title}
+                    onChange={(e) =>
+                      setFormData({ ...formData, title: e.target.value })
+                    }
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="content" className="text-right font-bold">
+                    Content
+                  </Label>
+                  <Textarea
+                    id="content"
+                    placeholder="Content"
+                    className="col-span-3"
+                    value={formData.content}
+                    onChange={(e) =>
+                      setFormData({ ...formData, content: e.target.value })
+                    }
+                  />
+                </div>
               </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right font-bold">
-                  Content
-                </Label>
-                <Textarea
-                  id="content"
-                  placeholder="Content"
-                  className="col-span-3"
-                  onChange={(e) => setContent(e.target.value)}
-                />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button
-                className="bg-rose-800"
-                type="submit"
-                onClick={sendNotification}
-              >
-                Send Notification
-              </Button>
-            </DialogFooter>
+              <DialogFooter>
+                <Button className="bg-rose-800" type="submit">
+                  Send Notification
+                </Button>
+              </DialogFooter>
+            </form>
           </DialogContent>
         </Dialog>
       </div>
